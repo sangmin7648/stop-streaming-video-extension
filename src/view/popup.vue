e0496d82fc7e1b57462df7c4ab49e6e325b929d6
이상민 <smlee01@miridih.com>
2022-09-04 21:27:21 +0900

    feat : setup popup page

--- src/view/popup.vue
+++ src/view/popup.vue
@@ -1,27 +1,52 @@
 <template>
-  <div class="main_app">
-    <h1>Hello {{ msg }}</h1>
+  <h4 style="color: red">Stop Streaming Video - Beta v0.0.1</h4>
+  <hr />
+  <div class="home">
+    <p>
+      mode
+      <input type="radio" v-model="mode" value="BLACKLIST" checked /> blacklist
+      <input type="radio" v-model="mode" value="WHITELIST" /> whitelist
+    </p>
+
+    <p>Provider : {{ provider }}</p>
+
+    <p>category list <input type="text" v-model="categoryList" /></p>
+    <p>channel list <input type="text" v-model="channelList" /></p>
+
+    <p><button @click="canWatch">Test</button></p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "popupView",
+  name: "HomeView",
   data() {
     return {
-      msg: "popup",
+      categoryList: "",
+      channelList: "",
+      mode: "",
+      provider: "Youtube",
     };
+  },
+  methods: {
+    canWatch() {
+      console.log("send");
+      axios
+        .get(
+          "http://localhost:8080/v1/watch-verifier/user/b9a87e60-2585-46e7-b9a5-b88948697a76/WHITELIST/verify-watch?provider=YOUTUBE&videoId=ypP4FgztjLI"
+        )
+        .then()
+        .catch();
+    },
   },
 };
 </script>
 
 <style>
-.main_app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  width: 15rem;
+  height: 20rem;
 }
 </style>
