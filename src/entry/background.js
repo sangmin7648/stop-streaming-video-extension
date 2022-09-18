12c8fc1e637e32ceb5c727034578d728f5e21d5a
Sangmin Lee <sangminlee7648@gmail.com>
2022-09-18 23:58:38 +0900

    feat: call verify watch api on tab update

--- src/entry/background.js
+++ src/entry/background.js
@@ -1,33 +1,35 @@
-import { parseYoutubeVideoId } from "@/service/providerparser.js";
+import { parseProvider, parseVideoId } from "@/service/providerparser.js";
 
-function verifyWatch(mode, provider, videoId, property) {
-  // query params: provider, videoId, property
-  // path params : mode
-  // return response
-  // using fetch api
-  return fetch(
-    `http://localhost:8080/v1/watch-verifier/${mode}/verify-watch?provider=${provider}&videoId=${videoId}&property=${property}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-}
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>
+  verifyWatch(tabId, changeInfo, tab)
+);
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+function verifyWatch(tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
-    if (tab.url.indexOf("youtube.com/watch?") != -1) {
-      // verify watch
-      verifyWatch(
-        "youtube",
-        "youtube",
-        parseYoutubeVideoId(tab.url),
-        "title"
-      ).then((response) => {
-        console.log(response);
-      });
+    const provider = parseProvider(tab.url);
+    if (provider == "YOUTUBE" && tab.url.includes("watch")) {
+      const videoId = parseVideoId(tab.url);
+      const properties = [];
+      verifyWatchRequest("WHITELIST", provider, videoId, properties).then(
+        (response) => {
+          console.log(response);
+        }
+      );
     }
   }
-});
+}
+
+function verifyWatchRequest(mode, provider, videoId, properties) {
+  return fetch(`http://localhost:8080/v1/watch-verifier/verify-watch`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      mode,
+      provider,
+      videoId,
+      properties,
+    }),
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store/providerstore";
+import store from "./store/watchverifierconditionstore";
 
 createApp(App).use(store).use(router).mount("#app");
